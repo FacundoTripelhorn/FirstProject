b85d5c6e94d51d501e1829275115dfd57a3358f3
Facundo Tripelhorn <facundo.tripelhorn@outlook.com>
2019-07-13 19:44:42 -0300

    Docker

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,11 +5,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FirstProject.Models;
+using FirstProject.Entities;
 
 namespace FirstProject.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            dbContext = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -28,7 +36,23 @@ namespace FirstProject.Controllers
 
         public IActionResult Products()
         {
-            return View();
+            dbContext.Database.EnsureCreated();
+
+            List<ProductViewModel> products = new List<ProductViewModel>();
+
+            foreach(var product in dbContext.Products)
+            {
+                ProductViewModel vm = new ProductViewModel
+                {
+                    Name = product.Name,
+                    Price = product.Price,
+                    Stock = product.Stock
+                };
+
+                products.Add(vm);
+            }
+
+            return View(products);
         }
     }
 }
--- Models/ProductViewModel.cs
+++ Models/ProductViewModel.cs
@@ -4,7 +4,6 @@ namespace FirstProject.Models
 {
     public class ProductViewModel
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
